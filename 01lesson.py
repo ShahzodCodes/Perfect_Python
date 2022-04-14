9d3111f18152b48197f036d1cdea76da33f55ca4
shakhzod2003 <yshahzod0@gmail.com>
2022-04-14 22:02:47 +0500

    lesson 01 is done

---
+++ 01lesson.py
@@ -0,0 +1,8 @@
+
+
+"""
+- I have downloaded Python to my PC 💻.
+- Every thing is ready to get started 🔥.
+- use https://mohirdev.uz/courses/python for more.
+- use https://python.sariq.dev to get more.
+"""
